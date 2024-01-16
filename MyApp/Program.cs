e35708a012a29a6584c092ec533834cc9517f5cb
Atul Kamble <atul_kamble@hotmail.com>
2024-01-16 13:45:45 +0530

    modified program.cs file

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello from Azure Training");
+Console.WriteLine("Hello from Azure Training new");
 Console.WriteLine("The current time is " + DateTime.Now);
--- MyApp/obj/Debug/net7.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net7.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca6627578e0b0228b099d59386d57bc03545ee5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb05eff8c3e61eace55b55a01e4ee8a0f6f4f4dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
